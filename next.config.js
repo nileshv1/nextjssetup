c64437c5491fa682537cf84b22bc47097fc6d55f
hxvarisn <hxvarisn@domesticandgeneral.com>
2023-07-14 12:13:07 +0530

    next js appliction

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const path = require('path')
+const nextConfig = {}
+
+module.exports = nextConfig
+
+module.exports = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+}
