a4c6cddece3a67dc83347bea6a8c794126e9133d
hxvarisn <hxvarisn@domesticandgeneral.com>
2023-07-14 12:43:21 +0530

    name changed

--- pages/home/home.jsx
+++ pages/home/home.jsx
@@ -2,7 +2,7 @@
 import variables from '../../styles/variables.module.scss'
 
 const Home = () => {
-  return <h1 style={{ color: variables.primaryColor }}>hello</h1>
+  return <h1 style={{ color: variables.primaryColor }}>seema</h1>
 };
 
 export default Home;
